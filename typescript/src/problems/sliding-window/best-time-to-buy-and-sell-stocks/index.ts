2d009bfca69de412099a02344953eb3d02ad8cb9
AkashSDas <aakashdas368@gmail.com>
2024-05-05 20:40:35 +0530

    [typescript] add best time to buy and sell stock problem

---
+++ typescript/src/problems/sliding-window/best-time-to-buy-and-sell-stocks/index.ts
@@ -0,0 +1,30 @@
+function maxProfit(prices: number[]): number {
+    let prevMin = prices[0];
+    let profit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < prevMin) {
+            prevMin = prices[i];
+        }
+
+        if (prices[i] - prevMin > profit) {
+            profit = prices[i] - prevMin;
+        }
+    }
+
+    return profit;
+}
+
+function maxProfit1(prices: number[]): number {
+    let profit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            if (prices[i] < prices[j] && prices[j] - prices[i] > profit) {
+                profit = prices[j] - prices[i];
+            }
+        }
+    }
+
+    return profit;
+}
